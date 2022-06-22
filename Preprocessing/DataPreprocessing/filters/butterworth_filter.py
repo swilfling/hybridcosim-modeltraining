c1011374963dc1c2f8f142c29170fabbb1a856ac
swilfling <sandra.wilfling@student.tugraz.at>
2022-06-22 10:04:03 +0200

    add filter type

--- Preprocessing/DataPreprocessing/filters/butterworth_filter.py
+++ Preprocessing/DataPreprocessing/filters/butterworth_filter.py
@@ -5,14 +5,15 @@ from .filter import Filter
 
 class ButterworthFilter(Filter):
     """
-    Butterworth lowpass filter for data smoothing.
+    Butterworth filter for data smoothing.
     """
     T = 10
     order = 2
+    filter_type = 'lowpass'
 
-    def __init__(self, T=10, order=2, **kwargs):
+    def __init__(self, T=10, order=2, filter_type='lowpass', **kwargs):
         super().__init__(**kwargs)
-        self._set_attrs(T=T, order=order)
+        self._set_attrs(T=T, order=order, filter_type=filter_type)
 
     def _fit(self, X, y=None, **fit_params):
-        return sig.butter(self.order, 1 / self.T)
+        return sig.butter(self.order, 1 / self.T, btype=self.filter_type)
--- Preprocessing/DataPreprocessing/filters/chebyshev_filter.py
+++ Preprocessing/DataPreprocessing/filters/chebyshev_filter.py
@@ -5,15 +5,16 @@ from .filter import Filter
 
 class ChebyshevFilter(Filter):
     """
-       Chebyshev lowpass filter for data smoothing.
+       Chebyshev filter for data smoothing.
     """
     T = 10
     order = 2
     ripple = 0.1
+    filter_type = 'lowpass'
 
-    def __init__(self, T=10, order=2, ripple=0.1, **kwargs):
+    def __init__(self, T=10, order=2, ripple=0.1, filter_type='lowpass', **kwargs):
         super().__init__(**kwargs)
-        self._set_attrs(T=T, order=order, ripple=ripple)
+        self._set_attrs(T=T, order=order, ripple=ripple, filter_type=filter_type)
 
     def _fit(self, X, y=None, **fit_params):
-        return sig.cheby1(self.order, self.ripple, 1 / self.T)
+        return sig.cheby1(self.order, self.ripple, 1 / self.T, btype=self.filter_type)
