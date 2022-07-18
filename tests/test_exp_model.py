2a6c3620a704a6d7898bcff929b35bdba5d4a3b0
swilfling <sandra.wilfling@student.tugraz.at>
2022-07-18 13:49:11 +0200

    adapt tests

--- tests/test_exp_model.py
+++ tests/test_exp_model.py
@@ -1,36 +1,38 @@
 from ModelTraining.datamodels.datamodels.processing.datascaler import DataScaler, Normalizer
 from ModelTraining.feature_engineering.expandedmodel import ExpandedModel, TransformerSet
-from ModelTraining.feature_engineering.feature_expanders import FeatureExpansion
+from ModelTraining.feature_engineering.feature_expanders import FeatureExpansion, PolynomialExpansion, IdentityExpander
 from ModelTraining.feature_engineering.feature_selectors import FeatureSelector
-from ModelTraining.datamodels.datamodels import Model
+from ModelTraining.datamodels.datamodels import Model, RandomForestRegression
+
+
+def test_instantiate_transformerset_feat_sel():
+    selector_mic = FeatureSelector.from_name('MICThreshold')
+    selector_r = FeatureSelector.from_name('RThreshold')
+
+    exp_params = {"degree": 3, "interaction_only": True, "include_bias": False}
+    expander = FeatureExpansion.from_name("PolynomialExpansion", **exp_params)
+    list_transformers = [selector_mic, expander, selector_r]
+    transformer_set = TransformerSet(list_transformers)
+    assert(isinstance(transformer_set.get_transformer_by_name('polynomialexpansion'), PolynomialExpansion))
 
 
 def test_instantiate_expandedmodel():
-    model_type = "RandomForestRegression"
-    model = Model.from_name(model_type, x_scaler_class=DataScaler.cls_from_name('Normalizer'), name="Model",
-                            parameters={})
 
+    model = RandomForestRegression(x_scaler_class=Normalizer, name="RF",  parameters={})
     exp_params = {"degree": 3, "interaction_only": True, "include_bias": False}
-    expanders = [FeatureExpansion.from_name(name, **exp_params) for name in ['IdentityExpander', 'PolynomialExpansion']]
-    expander_set = TransformerSet(transformers=expanders)
+    expander = FeatureExpansion.from_name("PolynomialExpansion", **exp_params)
+    expander_set = TransformerSet(transformers=[expander])
     expanded_model = ExpandedModel(transformers=expander_set, model=model, feature_names=[])
-    estimator = expanded_model.get_estimator()
+    estimator = expanded_model.model
 
-
-def test_instantiate_expandedmodel_feat_sel():
-    selector_mic = FeatureSelector.from_name('MICThreshold')
-    selector_r = FeatureSelector.from_name('RThreshold')
-    selectors = [selector_mic, selector_r]
-
-    exp_params = {"degree": 3, "interaction_only": True, "include_bias": False}
-    expanders = [FeatureExpansion.from_name(name, **exp_params) for name in ['IdentityExpander', 'PolynomialExpansion']]
-    list_transformers = [expanders[0], selector_mic, expanders[1], selector_r]
-    transformer_set = TransformerSet(list_transformers)
+    expander_from_model = expanded_model.transformers.get_transformer_by_name('polynomialexpansion')
+    assert(isinstance(estimator, RandomForestRegression))
+    assert(isinstance(expander_from_model,PolynomialExpansion))
+    assert(expander_from_model == expander)
 
 
 if __name__ == "__main__":
     test_instantiate_expandedmodel()
-    test_instantiate_expandedmodel_feat_sel()
-
+    test_instantiate_transformerset_feat_sel()
 
 
