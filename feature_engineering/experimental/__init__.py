c9243e991c522e83fe3ac434580132286375172c
swilfling <sandra.wilfling@student.tugraz.at>
2022-07-26 15:44:45 +0200

    Merge pull request #12 from tug-cps/lin_models
    * Added lasso regression
    added submodule

--- feature_engineering/experimental/__init__.py
+++ feature_engineering/experimental/__init__.py
@@ -1,4 +1,4 @@
-from . import dynamicfeaturessamplecut
+from .dynamicfeaturessamplecut import DynamicFeaturesSampleCut
 from . import forward_selector
 from . import mic_r_threshold
 from . import selector_by_name
