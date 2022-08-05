47a35cea42b48acf1853b6ed79265a7341fd2695
swilfling <sandra.wilfling@student.tugraz.at>
2022-08-05 12:43:26 +0200

    add statistical features

--- tests/test_stat.py
+++ tests/test_stat.py
@@ -1,15 +1,25 @@
 from ModelTraining.dataimport import DataImport
-from ModelTraining.feature_engineering.timebasedfeatures import StatisticalFeatures
+from ModelTraining.feature_engineering.timebasedfeatures import StatisticalFeatures, StatisticalFeaturesNumpy
 import matplotlib.pyplot as plt
+
 
 if __name__ == "__main__":
     data = DataImport.load("../Configuration/DataImport/Resampled15min.json").import_data(
         "../../Data/AEE/Resampled15min")
     stat_feats = StatisticalFeatures(window_size=24)
     import datetime
+    print("Stat feats Start")
     print(datetime.datetime.now())
     data_stat = stat_feats.fit_transform(data[['TSolarRL','VDSolar']])
     print(datetime.datetime.now())
+    print("Stat feats end")
+
+    print("Stat feats NP start")
+    print(datetime.datetime.now())
+    stat_feats = StatisticalFeaturesNumpy(window_size=24)
+    data_stat = stat_feats.fit_transform(data[['TSolarRL','VDSolar']])
+    print(datetime.datetime.now())
+    print("Stat feats NP end")
 
     plt.figure()
     plt.plot(data['TSolarRL'][0:1000])
