7c509611d1bb49f0f0571c51838bc892ab51f589
swilfling <sandra.wilfling@student.tugraz.at>
2022-08-05 11:14:13 +0200

    add selector by name, fix test

--- feature_engineering/experimental/__init__.py
+++ feature_engineering/experimental/__init__.py
@@ -1,5 +1,4 @@
 from .dynamicfeaturessamplecut import DynamicFeaturesSampleCut
 from . import forward_selector
 from . import mic_r_threshold
-from . import selector_by_name
 from . import feature_selection
